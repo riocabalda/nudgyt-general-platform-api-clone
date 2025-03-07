501979102d2d905fc41a58bd74539461145e7a76
riocabalda <159091515+riocabalda@users.noreply.github.com>
2025-03-07 14:39:49 +0800

    Update socket-server.ts

--- src/websocket/socket-server.ts
+++ src/websocket/socket-server.ts
@@ -1,12 +1,13 @@
 import { Server, ServerOptions } from 'socket.io';
-import frontendConfig from '../config/frontend.config';
+// import frontendConfig from '../config/frontend.config';
 import { authenticateWs } from '../middlewares/authenticate-ws';
 
 const socketConfig: Partial<ServerOptions> = {
   transports: ['websocket', 'polling'],
   pingTimeout: 60000,
   cors: {
-    origin: frontendConfig.url,
+    // origin: frontendConfig.url,
+    origin: '*',
     methods: ['GET', 'POST'],
     credentials: true
   }
